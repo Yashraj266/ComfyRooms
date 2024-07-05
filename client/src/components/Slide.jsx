776a6f3b631686dc18633d59b47fcb2335f1a810
Yash Raj <yashcoding26@gmail.com>
2024-07-05 11:42:57 +0530

    adding new project to github

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,16 @@
+import "../styles/Slide.scss";
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <slider>
+        <h1>
+          Welcome Home! Anywhere you roam <br /> Stay with comfort . Make your
+          memories
+        </h1>
+      </slider>
+    </div>
+  );
+};
+
+export default Slide;
